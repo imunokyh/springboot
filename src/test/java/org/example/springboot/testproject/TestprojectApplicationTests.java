4057c273c2bfaa61ebe5144a832a25fe711f7b26
imunokyh <dbsgh0180@gmail.com>
2025-07-20 14:34:27 +0900

    Hello World Example

---
+++ src/test/java/org/example/springboot/testproject/TestprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springboot.testproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
